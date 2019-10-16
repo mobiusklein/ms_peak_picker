cfd161ad82a8e10cff2cefa85c2d5ee47d1faa61
Joshua Klein <jklein@gritstone.com>
2019-10-16 10:53:06 -0400

    feature: Add mirror plot for pairs of peak sets

--- ms_peak_picker/plot.py
+++ ms_peak_picker/plot.py
@@ -29,10 +29,10 @@ def draw_raw(mz_array, intensity_array=None, ax=None, normalize=False, **kwargs)
     ax : :class:`matplotlib.Axes`, optional
         The axis to draw the plot on. If missing, a new one will be created using
         :func:`matplotlib.pyplot.subplots`
-    pretty: bool, optional
-        If `True`, will call :func:`_beautify_axes` on `ax`
     normalize: bool, optional
         if `True`, will normalize the abundance dimension to be between 0 and 100%
+    pretty: bool, optional
+        If `True`, will call :func:`_beautify_axes` on `ax`
     **kwargs
         Passed to :meth:`matplotlib.Axes.plot`
 
@@ -55,7 +55,7 @@ def draw_raw(mz_array, intensity_array=None, ax=None, normalize=False, **kwargs)
             set_ylim = intensity_array.min() >= 0
         else:
             set_ylim = True
-        _beautify_axes(ax, set_ylim)
+        _beautify_axes(ax, set_ylim, normalize)
     return ax
 
 
@@ -119,10 +119,10 @@ def draw_peaklist(peaklist, ax=None, normalize=False, **kwargs):
     ax : matplotlib.Axes, optional
         The axis to draw the plot on. If missing, a new one will be created using
         :func:`matplotlib.pyplot.subplots`
-    pretty: bool, optional
-        If `True`, will call :func:`_beautify_axes` on `ax`
     normalize: bool, optional
         if `True`, will normalize the abundance dimension to be between 0 and 100%
+    pretty: bool, optional
+        If `True`, will call :func:`_beautify_axes` on `ax`
     **kwargs
         Passed to :meth:`matplotlib.Axes.plot`
 
@@ -144,11 +144,74 @@ def draw_peaklist(peaklist, ax=None, normalize=False, **kwargs):
             set_ylim = intensity_array.min() >= 0
         else:
             set_ylim = True
-        _beautify_axes(ax, set_ylim)
+        _beautify_axes(ax, set_ylim, normalize)
     return ax
 
 
-def _beautify_axes(ax, set_ylim=True):
+def mirror_peaks(peaklist1, peaklist2, ax=None, normalize=True, kwargs1=None, kwargs2=None, **kwargs):
+    """Create a mirror plot of two peak lists.
+
+    Plot one peak list upside-down below another peak list. Commonly used to compare/contrast two
+    two peak lists visually.
+
+    Parameters
+    ----------
+    peaklist1 : :class:`~.Iterable`
+        The peak list to plot right-side up.
+    peaklist2 : :class:`~.Iterable`
+        The peak list to plot upside-down.
+    ax : matplotlib.Axes, optional
+        The axis to draw the plot on. If missing, a new one will be created using
+        :func:`matplotlib.pyplot.subplots`
+    normalize : bool, optional
+        Whether or not to normalize the intensity scales of the spectra.
+        The default is :const:`True`, unlike other drawing functions, in
+        order to keep the scales of the spectra relatively equal.
+    kwargs1 : dict, optional
+        Any keyword arguments to be applied solely when drawing `peaklist1`.
+    kwargs2 : dict, optional
+        Any keyword arguments to be applied solely when drawing `peaklist2`.
+    pretty: bool, optional
+        If `True`, will call :func:`_beautify_axes` on `ax`
+    **kwargs:
+        Keyword arguments to be used when drawing both peak lists.
+        Passed to :meth:`matplotlib.Axes.plot`
+
+    Returns
+    -------
+    :class:`matplotlib.Axes`
+    """
+    pretty = kwargs.pop("pretty", True)
+    if kwargs1 is None:
+        kwargs1 = {}
+    if kwargs2 is None:
+        kwargs2 = {}
+    if ax is None:
+        _, ax = plt.subplots(1)
+
+    # Draw first peak list
+    mz_array, intensity_array = peaklist_to_vector(peaklist1)
+    if normalize:
+        intensity_array = intensity_array / intensity_array.max() * 100.0
+    kwargs1.update(kwargs)
+    ax.plot(mz_array, intensity_array, **kwargs1)
+
+    # Draw second peak list
+    mz_array, intensity_array = peaklist_to_vector(peaklist2)
+    if normalize:
+        intensity_array = intensity_array / intensity_array.max() * 100.0
+    intensity_array = -1 * intensity_array
+    kwargs2.update(kwargs)
+    ax.plot(mz_array, intensity_array, **kwargs2)
+
+    # Update the axes if needed.
+    if pretty:
+        _beautify_axes(ax, False)
+        _normalize_ylabels(ax, adjust_sign=True)
+    return ax
+
+
+def _beautify_axes(ax, set_ylim=True, normalize=False):
     ax.axes.spines['right'].set_visible(False)
     ax.axes.spines['top'].set_visible(False)
     ax.yaxis.tick_left()
@@ -157,6 +220,23 @@ def _beautify_axes(ax, set_ylim=True):
     ax.get_xaxis().get_major_formatter().set_useOffset(False)
     if set_ylim:
         ax.set_ylim(0, max(ax.get_ylim()))
+    if normalize:
+        _normalize_ylabels(ax)
+    return ax
+
+
+def _normalize_ylabels(ax, adjust_sign=True):
+    ytick_labels = ax.get_yticklabels()
+    yticks = ax.get_yticks()
+    for tl, tick in zip(ytick_labels, yticks):
+        txt = tl.get_text()
+        if txt == "":
+            txt = str(tick)
+        if adjust_sign:
+            txt = txt.replace("-", "")
+        txt += "%"
+        tl.set_text(txt)
+    ax.set_yticklabels(ytick_labels)
     return ax
 
 
