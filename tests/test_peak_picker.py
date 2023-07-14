6f8fa5e75f00b972570e89fba6fef4ee162ec3df
Joshua Klein <mobiusklein@gmail.com>
2023-07-14 08:37:02 -0400

    Update tests

--- tests/test_peak_picker.py
+++ tests/test_peak_picker.py
@@ -1,13 +1,20 @@
 import pickle
 
-from ms_peak_picker.peak_picker import pick_peaks
+from ms_peak_picker.peak_picker import pick_peaks, find_signal_to_noise
 
 def test_peak_picker():
     with open('tests/data/arrays.pkl', 'rb') as fh:
         data = pickle.load(fh)
 
-    mz = data['mz']
-    intensity = data['intensity']
-    peaks = pick_peaks(mz, intensity)
-    assert len(peaks) == 2108
-    assert abs(sum(p.intensity for p in peaks) - 4531158140.658203) < 1e-3
+    mz_array = data['mz']
+    intensity_array = data['intensity']
+
+    assert find_signal_to_noise(
+        intensity_array[15194], intensity_array, 15194) > 0
+
+    assert find_signal_to_noise(
+        intensity_array[15197], intensity_array, 15197) == 0
+
+    peaks = pick_peaks(mz_array, intensity_array)
+    assert len(peaks) == 2107
+    assert abs(sum(p.intensity for p in peaks) - 4531125399.828125) < 1e-3
